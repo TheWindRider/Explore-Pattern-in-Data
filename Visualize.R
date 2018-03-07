560bf53dd887a47b6d0f764471ed72d8689faccb
Chenguang Yang <ychg235711@gmail.com>
2018-03-06 21:41:52 -0800

    Add generalized multi-var plot
    Not working perfectly

--- Visualize.R
+++ Visualize.R
@@ -1,7 +1,9 @@
 library(tourr)
+library(gpairs)
 
 # 2D visualization
 plot(data_set)
+gpairs(data_set, outer.rot = c(90,0))  # some types of plots won't display
 
 # 3D visualization
 animate(data_set, fps=15)
